d3ebab70a1ae16e9bae5f09ce6d7e66259258596
Michael Di Prisco <cadienvan@gmail.com>
2023-01-21 15:56:53 +0100

    feat: first commit

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,44 @@
-import { sayHello } from '.';
+import { randomDelay } from './index';
+import { performance } from 'perf_hooks';
 
-it('should display "Hello World!"', () => {
-  expect(sayHello()).toBe('Hello World!');
+describe('throws', function () {
+  it('should throw if method is not sync or async', function () {
+    expect(() => randomDelay({ method: 'foo' })).toThrow();
+  });
+  it('should throw if minDelay is negative', function () {
+    expect(() => randomDelay({ minDelay: -1 })).toThrow();
+  });
+  it('should throw if maxDelay is negative', function () {
+    expect(() => randomDelay({ maxDelay: -1 })).toThrow();
+  });
+  it('should throw if minDelay > maxDelay', function () {
+    expect(() => randomDelay({ minDelay: 1000, maxDelay: 500 })).toThrow();
+  });
+});
+
+describe('sync', function () {
+  it('should wait for randomDelay', function (done) {
+    const now = performance.now();
+    const options = {
+      minDelay: 1000,
+      maxDelay: 2000,
+      method: 'sync'
+    };
+    randomDelay(options);
+    expect(performance.now()).toBeGreaterThan(now + options.minDelay);
+    done();
+  });
+});
+
+describe('async', function () {
+  it('should wait for randomDelay', async function () {
+    const now = performance.now();
+    const options = {
+      minDelay: 1000,
+      maxDelay: 2000,
+      method: 'async'
+    };
+    await randomDelay(options);
+    expect(performance.now()).toBeGreaterThan(now + options.minDelay);
+  });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,49 @@
-import { HelloWorld } from './hello-world';
+import sleep from 'event-loop-sleep';
 
-export const sayHello = () => {
-  return HelloWorld;
+export type Options = {
+  minDelay?: number;
+  maxDelay?: number;
+  method: string;
 };
+
+const defaultOptions = {
+  minDelay: 0,
+  maxDelay: 1000,
+  method: 'async'
+};
+
+export function randomDelay(options: Partial<Options> = {}) {
+  const { minDelay, maxDelay, method } = { ...defaultOptions, ...options };
+  if (method !== 'sync' && method !== 'async')
+    throw new Error("randomDelay method must be 'sync' or 'async'");
+  if (minDelay < 0 || maxDelay < 0)
+    throw new Error('randomDelay minDelay and maxDelay must be >= 0');
+  if (minDelay > maxDelay)
+    throw new Error('randomDelay minDelay must be <= maxDelay');
+
+  const delay = Math.random() * (maxDelay - minDelay) + minDelay;
+  if (options.method === 'sync') {
+    sleep(delay);
+    return true;
+  } else {
+    return new Promise((resolve) => {
+      setTimeout(resolve, delay);
+    });
+  }
+}
+
+export function randomDelayed(
+  fn: (...args) => any,
+  options: Options = defaultOptions
+) {
+  if (options.method === 'sync')
+    return (...args) => {
+      randomDelay(options);
+      return fn(...args);
+    };
+  else
+    return async (...args) => {
+      await randomDelay(options);
+      return fn(...args);
+    };
+}
